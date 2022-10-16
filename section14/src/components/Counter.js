1e176077e0f698ee12b1d7c3df02fb53ba59b8a8
jeongsehun <tpgns933@gmail.com>
2022-10-16 23:40:13 +0900

    10.16

--- section14/src/components/Counter.js
+++ section14/src/components/Counter.js
@@ -1,27 +1,68 @@
 import { useSelector, useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
+import { counterAction } from '../store/counter';
 
 const Counter = () => {
   const dispatch = useDispatch();
-  // const [counter, showCounter] = useSelector(state => [state.counter, state.showCounter]);
-  const [counter, showCounter] = useSelector(state => [state.counter, state.showCounter]);
+  const counter = useSelector(state => state.counter);
+  const AMOUNT = 2;
 
-  const toggleCounterHandler = () => dispatch({ type: 'toggleCounter' });
-  const incrementHandler = () => dispatch({ type: 'Increment' });
-  const increaseHandler = () => dispatch({ type: 'Increase', amount: 5 });
-  const decrementHandler = () => dispatch({ type: 'Decrement' });
+  const toggleCounterHandler = () => dispatch(counterAction.toggleCounter());
+  const incrementHandler = () => dispatch(counterAction.increment());
+  const increaseHandler = () => dispatch(counterAction.increase({ AMOUNT }));
+  const decrementHandler = () => dispatch(counterAction.decrement());
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}> {showCounter && counter} </div>
+      <div className={classes.value}> {counter.showCounter && counter.counter} </div>
       <div>
         <button onClick={decrementHandler}>Decrease</button>
         <button onClick={incrementHandler}>Increase</button>
-        <button onClick={increaseHandler}>Increase of 5</button>
+        <button onClick={increaseHandler}>Increase of {AMOUNT}</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
 };
-
 export default Counter;
+
+// class Counter extends Component {
+//   incrementHandler() {
+//     this.props.increment();
+//   }
+
+//   decrementHandler() {
+//     this.props.decrement();
+//   }
+
+//   toggleCounterHandler() {}
+
+//   render() {
+//     return (
+//       <main className={classes.counter}>
+//         <h1>Redux Counter</h1>
+//         <div className={classes.value}>{this.props.counter}</div>
+//         <div>
+//           <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+//           <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+//         </div>
+//         <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+//       </main>
+//     );
+//   }
+// }
+
+// const mapStateToProps = state => {
+//   return {
+//     counter: state.counter
+//   };
+// }
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     increment: () => dispatch({ type: 'increment' }),
+//     decrement: () => dispatch({ type: 'decrement' }),
+//   }
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- section14/src/components/Header.js
+++ section14/src/components/Header.js
@@ -1,22 +1,30 @@
 import classes from './Header.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { authAction } from '../store/auth';
 
 const Header = () => {
+  const authDispatch = useDispatch();
+  const logoutHandler = () => authDispatch(authAction.logout());
+  const isAuth = useSelector(state => state.auth);
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
+      {isAuth.isAuthenticated && (
+        <nav>
+          <ul>
+            <li>
+              <a href="/">My Products</a>
+            </li>
+            <li>
+              <a href="/">My Sales</a>
+            </li>
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
